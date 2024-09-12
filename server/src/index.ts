203d1380e06d7588374dc06fbe8f7fc48c3807ca
emtom <malysz.tomasz@gmail.com>
2024-09-12 20:15:38 +0200

    initial server setup

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+// CONFIG
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES HERE TBD
+app.get("/hello", (req, res) => {
+  res.send('YO!');
+})
+
+// SERVER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log('SERVER READING ON PORT', port);
+});
