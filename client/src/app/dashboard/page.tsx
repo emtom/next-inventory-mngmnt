13701ecda032c0c3fed3c36aaf399f7f26289f90
emtom <malysz.tomasz@gmail.com>
2024-09-30 12:47:18 +0200

    feat: add Dashboard cards

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,32 @@
+'use client'
+
+import CardPopularProducts from "./card-popular-products";
+import CardPurchaseSummary from "./card-purchase-summary";
+import CardSalesSummary from "./card-sales-summary";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+      <CardSalesSummary />
+      <CardPurchaseSummary />
+
+      <div className="bg-gray-500 row-span-3">
+        
+      </div>
+
+      <div className="bg-gray-500 md:row-span-1 xl:row-span-2">
+        
+      </div>
+
+      <div className="bg-gray-500 md:row-span-1 xl:row-span-2">
+        
+      </div>
+
+      <div className="bg-gray-500 md:row-span-1 xl:row-span-2">
+        
+      </div>
+    </div>
   )
 }
 
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,62 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface IProduct {
+  productId: string;
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
+export interface ISalesSummary {
+  salesSummaryId: string;
+  totalValue: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface IPurchaseSummary {
+  purchaseSummaryId: string;
+  totalPurchased: number;
+  changePercengate?: number;
+  date: string;
+}
+
+export interface IExpenseSummary {
+  expenseSummaryId: string;
+  totalExpenses: number;
+  date: string;
+}
+
+export interface IExpenseByCategorySummary {
+  expenseByCategorySummaryId: string;
+  category: string;
+  amount: string;
+  date: string;
+}
+
+export interface IDashboardMetrics {
+  popularProducts: IProduct[];
+  salesSummary: ISalesSummary[];
+  purchaseSummary: IPurchaseSummary[];
+  expenseSummary: IExpenseSummary[];
+  expenseByCategorySummary: IExpenseByCategorySummary[];
+}
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL
   }),
   reducerPath: 'api',
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  tagTypes: ['DashboardMetrics'],
+  endpoints: (build) => ({
+    getDashboardMetrics: build.query<IDashboardMetrics, void>({
+      query: () => '/dashboard',
+      providesTags: ['DashboardMetrics']
+    })
+  }),
 });
 
-export const {} = api;
+export const {
+  useGetDashboardMetricsQuery,
+} = api;
