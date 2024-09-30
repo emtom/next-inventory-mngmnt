056d732fe3c3667caf182bdc34f38605584434e3
emtom <malysz.tomasz@gmail.com>
2024-09-30 22:22:32 +0200

    chore: remove now ignored directories

--- server/dist/src/routes/dashboard.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboard_1 = require("../controllers/dashboard");
-const router = (0, express_1.Router)();
-router.get('/', dashboard_1.getDashboardMetricts);
-exports.default = router;
